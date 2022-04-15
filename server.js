3192f3e2efc6fe29633f73a0cbe77b2d8e6399ab
mikak <mekawaters@yahoo.com>
2022-04-15 17:41:15 -0400

    started post, also messing around with ejs which i already regret

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
 // Requirements for express
 const express = require('express');
 const app = express();
-const path = require('path');
+// view engine
+app.set('view engine', 'ejs');
 
 // why can't this be found!!!! i blame nodemon
-const { notes } =require('./Develop/db/db.json')
+const notes = require('./Develop/db/db');
+
 
 // port will be 3001 for local, but can be changed for Heroku
 const port = process.env.PORT || 3001;
 
 // allows js/css from public
-app.use(express.static('./public'));
+// app.use(express.static('./public'));
+
+// testing route
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+// post
+app.post('/api/notes', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+});
 
 
-// testing localhost
-app.get('/', (req, res) => {
-    res.send('Hello')
-})
-
-// trying to create route
-app.get('notes', (req, res) => {
-    res.json(db)
-})
-
-
-// 
+// shows server is working
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
