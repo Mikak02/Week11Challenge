eaf4ebf3221e252489e1368e1701ed3c85b59df7
mikak <mekawaters@yahoo.com>
2022-04-15 15:31:50 -0400

    set up port, 1st attempt at creating path to public

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
+// Requirements for express
 const express = require('express');
-const { get } = require('express/lib/response');
 const app = express();
-const port = process.env.PORT || 3001
-const { notes } = require('./db/db');
+const path = require('path');
 
-app.use(express.static('public'));
+// why can't this be found!!!! i blame nodemon
+const { notes } =require('./Develop/db/db.json')
+
+// port will be 3001 for local, but can be changed for Heroku
+const port = process.env.PORT || 3001;
+
+// allows js/css from public
+app.use(express.static('./public'));
+
+
+// testing localhost
 app.get('/', (req, res) => {
-    res.send('Hello there!')
+    res.send('Hello')
+})
+
+// trying to create route
+app.get('notes', (req, res) => {
+    res.json(db)
 })
 
 
-
+// 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
